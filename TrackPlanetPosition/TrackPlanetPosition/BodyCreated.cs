d0fad194deeb22175d1365c4f65c922b133ebfe0
Vimal Patel <vp@123insight.com>
2014-10-25 18:12:24 +0100

    Added

---
+++ TrackPlanetPosition/TrackPlanetPosition/BodyCreated.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TrackPlanetPosition
+{
+    public class BodyCreated : IMessage
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Colour Colour { get; set; }
+        public Point Location { get; set; }
+        public Point Vector { get; set; }
+        public decimal Radius { get; set; }
+    }
+
+    public class BodyMovement : IMessage
+    {
+        public string Id { get; set; }
+        public Point Location { get; set; }
+        public Point Velocity { get; set; }
+    }
+
+    public class Point
+    {
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+    }
+
+    public class Colour
+    {
+        public decimal R { get; set; }
+        public decimal G { get; set; }
+        public decimal B { get; set; }
+    }
+}
