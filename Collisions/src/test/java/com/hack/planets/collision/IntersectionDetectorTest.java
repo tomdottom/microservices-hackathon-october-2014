1e957fb415ddae85705b152e53e435464c230c47
Daniel Vaughan <dan@danielvaughan.com>
2014-10-25 17:39:14 +0100

    added intersectionDectector

--- Collisions/src/test/java/com/hack/planets/collision/IntersectionDetectorTest.java
+++ Collisions/src/test/java/com/hack/planets/collision/IntersectionDetectorTest.java
@@ -3,7 +3,8 @@ package com.hack.planets.collision;
 import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
 
 /**
  * Created by danielvaughan on 25/10/14.
@@ -19,10 +20,17 @@ public class IntersectionDetectorTest {
     }
 
     @Test
-    public void test()
+    public void not_detect_collision_for_no_movement()
     {
-        assertTrue(intersectionDetector.isCollision(new Movement(new Position(0,0), new Position(0,0)), new Movement(new Position(0,0), new Position(0,0))));
+        assertFalse(intersectionDetector.isCollision(new Movement(new Position(1, 1), new Position(0, 0)), new Movement(new Position(1, 1), new Position(0, 0))));
     }
 
+    @Test
+    public void detect_collision_for_overlapping_paths()
+    {
+        assertTrue(intersectionDetector.isCollision(new Movement(new Position(0, 0), new Position(1, 1)), new Movement(new Position(1, 0), new Position(0, 1))));
+        Position intersection = intersectionDetector.getIntersection();
+                System.out.println(intersection);
+    }
 
 }
