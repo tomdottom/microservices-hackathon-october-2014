1677295c1e55611b2db2b91abfcb604e42f4e2a0
Derek M. Jones <plagchk@Joes-MacBook-Pro.local>
2014-10-25 17:15:32 +0100

    With added comments

--- forces/planet_force.py
+++ forces/planet_force.py
@@ -11,6 +11,7 @@ def gravity_force(m1, m2, dist):
    return G*m1*m2/math.pow(dist, 2)
 
 
+# Calculate force vector between two planets
 def planet_vector(me_p, other_p):
    p_dist=math.sqrt(pow(me_p[0]-other_p[0], 2)+pow(me_p[1]-other_p[1], 2))
    # print p_dist
@@ -22,6 +23,8 @@ def planet_vector(me_p, other_p):
    return [x_vect, y_vect]
 
 
+# Loop over all planets
+# Calculate total force experienced by every planet
 def all_forces(the_universe, planet_back, universe_back):
    for my_planet in the_universe:
       my_vector=[0, 0]
@@ -30,6 +33,7 @@ def all_forces(the_universe, planet_back, universe_back):
             a_vector=planet_vector(my_planet, other_planet)
             my_vector[0]+=a_vector[0]
             my_vector[1]+=a_vector[1]
+# Pass information to planet callback
       planet_back([my_vector[0], my_vector[1], my_planet[3]])
    universe_back()
    return
